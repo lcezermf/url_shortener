c474fbd20ec8d130a412b2f6603203a8b461c28e
Luiz Cezer <lccezinha@gmail.com>
2022-10-18 17:59:08 -0300

    Improve testing by using describe

--- test/url_shortener/shortener_test.exs
+++ test/url_shortener/shortener_test.exs
@@ -4,48 +4,56 @@ defmodule UrlShortener.ShortenerTest do
   alias UrlShortener.URL
   alias UrlShortener.Shortener
 
-  test "must return a valid genserver" do
-    {:ok, pid} = Shortener.start_link()
+  describe "start_link/0" do
+    test "must return a valid genserver" do
+      {:ok, pid} = Shortener.start_link()
 
-    assert is_pid(pid)
-    assert Process.info(pid)[:registered_name] == :shortener_server
+      assert is_pid(pid)
+      assert Process.info(pid)[:registered_name] == :shortener_server
+    end
   end
 
-  test "must return the url short version" do
-    {:ok, _pid} = Shortener.start_link()
+  describe "shorten/1" do
+    test "must return the url short version" do
+      {:ok, _pid} = Shortener.start_link()
 
-    url = "http://www.pudim.com.br"
-    %URL{} = result = Shortener.shorten(url)
+      url = "http://www.pudim.com.br"
+      %URL{} = result = Shortener.shorten(url)
 
-    assert result.original == url
-    assert result.count == 1
+      assert result.original == url
+      assert result.count == 1
+    end
   end
 
-  test "must return state with all urls" do
-    {:ok, _pid} = Shortener.start_link()
+  describe "get_urls/0" do
+    test "must return state with all urls" do
+      {:ok, _pid} = Shortener.start_link()
 
-    url = "http://www.pudim.com.br"
-    Shortener.shorten(url)
+      url = "http://www.pudim.com.br"
+      Shortener.shorten(url)
 
-    [first_url | _] = urls = Shortener.get_urls()
+      [first_url | _] = urls = Shortener.get_urls()
 
-    assert is_list(urls)
-    assert first_url.original == "http://www.pudim.com.br"
-    assert length(urls) == 1
+      assert is_list(urls)
+      assert first_url.original == "http://www.pudim.com.br"
+      assert length(urls) == 1
+    end
   end
 
-  test "must reset the state and clean all urls" do
-    {:ok, _pid} = Shortener.start_link()
+  describe "clear/0" do
+    test "must reset the state and clean all urls" do
+      {:ok, _pid} = Shortener.start_link()
 
-    url = "http://www.pudim.com.br"
-    Shortener.shorten(url)
-    urls = Shortener.get_urls()
+      url = "http://www.pudim.com.br"
+      Shortener.shorten(url)
+      urls = Shortener.get_urls()
 
-    assert length(urls) == 1
+      assert length(urls) == 1
 
-    Shortener.clear()
+      Shortener.clear()
 
-    urls = Shortener.get_urls()
-    assert Enum.empty?(urls)
+      urls = Shortener.get_urls()
+      assert Enum.empty?(urls)
+    end
   end
 end
